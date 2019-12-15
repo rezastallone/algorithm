86e4020f1b7feadb92690604d8f717f5fde2ca3c
Reza Stallone <reza.stallone@dana.id>
2019-12-15 14:33:30 +0700

    update files

--- src/datetry.kt
+++ src/datetry.kt
@@ -2,23 +2,23 @@ import java.util.*
 
 fun main() {
 
-    //simulasi bahwa install dana tuh 30 hari yang lalu
+    //simulasi bahwa install tuh 30 hari yang lalu
     val waktuPertamaInstallDana = Calendar.getInstance()
 //    waktuPertamaInstallDana.timeInMillis = waktuPertamaInstallDana.timeInMillis - ( 86400000L * 30 )
     waktuPertamaInstallDana.timeInMillis = waktuPertamaInstallDana.timeInMillis
 
 
-    //simulasi hari ini tuh 25 hari setelah pertama install dana, jadi 5 hari lagi harusnya skejulnya nyala
+    //simulasi hari ini tuh 25 hari setelah pertama install, jadi 5 hari lagi harusnya skejulnya nyala
     val waktuSekarang = Calendar.getInstance()
 //    waktuSekarang.timeInMillis = waktuSekarang.timeInMillis - ( 86400000L * 10 )
     waktuSekarang.timeInMillis = waktuSekarang.timeInMillis
 
 
     //eh kita nyalain hp nih, nah perlu skejul ulang kapan harusnya alarm ke trigger,
-    //kita kalkulasi dari selisih hari waktu sekarang dengan waktu pertama install dana
+    //kita kalkulasi dari selisih hari waktu sekarang dengan waktu pertama install
     //kemudian jumlah hari tersebut di bagi dengan interval
     //kita akan dapat nilai magnitude yang akan dikalikan dengan interval hari
-    //hasil perkalian itu akan ditambahkan dengan millis dari hari pertama install dana
+    //hasil perkalian itu akan ditambahkan dengan millis dari hari pertama install
     val intervalDay = 86400000
     val intervalConfig = 30
     val elapsedDay = ( waktuSekarang.timeInMillis - waktuPertamaInstallDana.timeInMillis ) / intervalDay.toDouble()
--- src/huntinggolum.kt
+++ src/huntinggolum.kt
@@ -1,102 +1,80 @@
-//import java.util.*
-//
-//
-//data class Coordinate(val x: Int, val y: Int)
-//
-//fun main() {
-//    val golumnLoc = golumnDummyMap()
-//    val aragonMap = aragonDummapMap()
-//    println(golumnLoc)
-//    println(aragonMap)
-//    aragonMap.forEach{
-//        val strToSearch = "${golumnLoc[0]}$$it"
-//        println("Search string : ${findMatchIndexes(strToSearch, golumnLoc[0].length+1)}")
-//    }
-//}
-//
-//
-//fun golumnDummyMap(): MutableList<String> {
-//    val col1 = "aba"
-//    val col2 = "bab"
-//    val col3 = "aba"
-//    return mutableListOf(col1, col2, col3)
-//}
-//
-//fun aragonDummapMap(): MutableList<String> {
-//    val col1 = "aababa"
-//    val col2 = "ababab"
-//    val col3 = "bababa"
-//    val col4 = "ababab"
-//    val col5 = "ababab"
-//    val col6 = "bababa"
-//    val col7 = "ababab"
-//    return mutableListOf(
-//        col1,
-//        col2,
-//        col3,
-//        col4,
-//        col5,
-//        col6,
-//        col7
-//    )
-//}
-//
-//fun getGolumnLocationDescription() {
-//    val reader = Scanner(System.`in`)
-//    val n1 = reader.nextInt()
-//    val n2 = reader.nextInt()
-//    val patternMatrix = MutableList(n1) {
-//        MutableList(n2) {
-//            0
-//        }
-//    }
-//
-//    println("recieved $n1 $n2, content are : ")
-//    for (col in patternMatrix) {
-//        for (row in col) {
-//            convertToIntListOrNull(row)
-//        }
-//        println()
-//    }
-//}
-//
-//fun findMatchIndexes(text: String, separatorIndex: Int): MutableList<Int> {
-//    val ZArray = MutableList(text.length) {
-//        0
-//    }
-//    var left = 0
-//    var right = 0
-//
-//    for (ZValueItr in 1 until text.length) {
-//        if (ZValueItr > right) {
-//            right = ZValueItr
-//            left = ZValueItr
-//            right = iterateMatchingPattern(right, text, left, ZArray, ZValueItr)
-//        } else {
-//            var leftMirrorItr = ZValueItr - left
-//            if (ZValueItr + ZArray[leftMirrorItr] > right) {
-//                left = ZValueItr
-//                right = iterateMatchingPattern(right, text, left, ZArray, ZValueItr)
-//            } else {
-//                ZArray[ZValueItr] = ZArray[leftMirrorItr]
-//            }
-//        }
-//    }
-//    return ZArray.subList(separatorIndex, ZArray.size)
-//}
-//
-//private fun iterateMatchingPattern(
-//    right: Int,
-//    text: String,
-//    left: Int,
-//    ZArray: MutableList<Int>,
-//    ZValueItr: Int
-//): Int {
-//    var right1 = right
-//    while (right1 < text.length && text[right1] == text[right1 - left]) {
-//        right1++
-//    }
-//    ZArray[ZValueItr] = ( right1 - left )
-//    right1--
-//    return right1
-//}
+data class Coordinate(val x: Int, val y: Int)
+
+fun main() {
+    val golumnLoc = golumnDummyMap()
+    val aragonMap = aragonDummapMap()
+    println(golumnLoc)
+    println(aragonMap)
+    aragonMap.forEach{
+        val strToSearch = "${golumnLoc[0]}$$it"
+        println("Search string : ${getMatchIndexes(strToSearch, golumnLoc[0].length+1)}")
+    }
+}
+
+
+fun golumnDummyMap(): MutableList<String> {
+    val col1 = "aba"
+    val col2 = "bab"
+    val col3 = "aba"
+    return mutableListOf(col1, col2, col3)
+}
+
+fun aragonDummapMap(): MutableList<String> {
+    val col1 = "aababa"
+    val col2 = "ababab"
+    val col3 = "bababa"
+    val col4 = "ababab"
+    val col5 = "ababab"
+    val col6 = "bababa"
+    val col7 = "ababab"
+    return mutableListOf(
+        col1,
+        col2,
+        col3,
+        col4,
+        col5,
+        col6,
+        col7
+    )
+}
+
+fun getMatchIndexes(text: String, separatorIndex: Int): MutableList<Int> {
+    val ZArray = MutableList(text.length) {
+        0
+    }
+    var left = 0
+    var right = 0
+
+    for (ZValueItr in 1 until text.length) {
+        if (ZValueItr > right) {
+            right = ZValueItr
+            left = ZValueItr
+            right = iterateMatchingPattern(right, text, left, ZArray, ZValueItr)
+        } else {
+            var leftMirrorItr = ZValueItr - left
+            if (ZValueItr + ZArray[leftMirrorItr] > right) {
+                left = ZValueItr
+                right = iterateMatchingPattern(right, text, left, ZArray, ZValueItr)
+            } else {
+                ZArray[ZValueItr] = ZArray[leftMirrorItr]
+            }
+        }
+    }
+    return ZArray.subList(separatorIndex, ZArray.size)
+}
+
+private fun iterateMatchingPattern(
+    right: Int,
+    text: String,
+    left: Int,
+    ZArray: MutableList<Int>,
+    ZValueItr: Int
+): Int {
+    var right1 = right
+    while (right1 < text.length && text[right1] == text[right1 - left]) {
+        right1++
+    }
+    ZArray[ZValueItr] = ( right1 - left )
+    right1--
+    return right1
+}
